2c0f99a59662553bb0874cf30a46a812c6f497b8
root <root@ip-172-31-44-112.us-west-2.compute.internal>
2014-12-23 01:52:46 +0000

    primer commit

---
+++ server.js
@@ -0,0 +1,27 @@
+  var http = require('http');
+  var soap = require('soap');
+  
+  var myService = {
+      MTServicesService: {
+          MTHTTPPort: {
+              requestBalanceByEntityPattern: function(args) {
+                var result = parseInt(args.a) + parseInt(args.b);
+                
+                  return {
+                      return: "00000000000000"
+                  }
+              }
+          }
+      }
+  }
+
+  var xml = require('fs').readFileSync('esquema.wsdl', 'utf8'),
+      server = http.createServer(function(request,response) {
+          response.end("404: Not Found: "+request.url)
+      });
+  
+
+  server.listen(3000);
+  soap.listen(server, '/wsdl', myService, xml);
+  console.log('work');
+  
