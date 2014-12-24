93e3667b530fec4f3f21e7ae7defddde18b55b14
Fernando Barrios <fer.b.1990@gmail.com>
2014-12-24 15:30:50 -0600

    retornar string

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
   var http = require('http');
   var soap = require('soap');
-  
+
   var myService = {
       MTServicesService: {
           MTHTTPPort: {
               requestBalanceByEntityPattern: function(args) {
                 var result = parseInt(args.a) + parseInt(args.b);
-                
-                  return {
-                      return: "00000000000000"
-                  }
+                return: "00000000000000"
               }
           }
       }
@@ -19,9 +16,9 @@
       server = http.createServer(function(request,response) {
           response.end("404: Not Found: "+request.url)
       });
-  
+
 
   server.listen(8000);
   soap.listen(server, '/wsdl', myService, xml);
   console.log('work');
-  
+
