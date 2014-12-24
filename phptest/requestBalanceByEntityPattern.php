a68a5e19656cf35105918e7356715a7c721b618b
Fernando Barrios <fer.b.1990@gmail.com>
2014-12-24 15:48:10 -0600

    prueba

--- phptest/requestBalanceByEntityPattern.php
+++ phptest/requestBalanceByEntityPattern.php
@@ -2,11 +2,11 @@
     ini_set("soap.wsdl_cache_enabled", "0");
     $client = new SoapClient("http://54.148.81.57:8000/wsdl?wsdl",array("trace"=>1));
     $transactionPattern = "000000000000111111";
-    /*$params = array(
-                  "transactionPattern"=>"2");*/
+    $params = array(
+                  "transactionPattern"=>"2");
 
     //$result=$client->BillingNotification($params['phonenumber'],$params['shortcodenumber'],$params['price'],$params['transactionid']);
-    $result=$client->requestBalanceByEntityPattern($transactionPattern);
+    $result=$client->requestBalanceByEntityPattern($params);
 
 ?>
 <!DOCTYPE html>
