ba07a6d2399922273df39a6526b429c9a7a1d631
kelvi padilla <kurtkelvisk88@gmail.com>
2014-12-30 22:51:29 -0600

    modificacion de wsdl similar a los ejemplos

--- phptest/creditBalanceByEntityPattern.php
+++ phptest/creditBalanceByEntityPattern.php
@@ -1,6 +1,6 @@
 <?php
     ini_set("soap.wsdl_cache_enabled", "0");
-    $client = new SoapClient("http://54.148.81.57:8000/wsdl?wsdl",array("trace"=>1));
+    $client = new SoapClient("http://localhost:8000/wsdl?wsdl",array("trace"=>1));
     $params = array(
                   "transactionPattern"=>"SENT111111");
     $result=$client->creditBalanceByEntityPattern($params);
--- server.js
+++ server.js
@@ -2,23 +2,29 @@
   var soap = require('soap');
 
   var myService = {
-      MTServicesService: {
-          MTHTTPPort: {
+      TransactionService: {
+          TransactionServicePort: {
               requestBalanceByEntityPattern: function(args) {
                 return {
-                  return: args.transactionPattern + "RESPONSE00000000"
+                  return: args.transactionPattern + "requestResponse00000000"
                 }
               },
               creditBalanceByEntityPattern: function (args) {
                 return {
-                  return: args.transactionPattern + "RESPONSE123123123123123"
+                  return: args.transactionPattern + "creditResponse123123123123123"
+                }
+              },
+              reverseBalanceByEntityPattern: function (args){
+                return {
+                  return: args.transactionPattern + "reverseResponse123123123123123"
                 }
               }
+              
           }
       }
   }
 
-  var xml = require('fs').readFileSync('esquema.wsdl', 'utf8'),
+  var xml = require('fs').readFileSync('./esquema.wsdl', 'utf8'),
       server = http.createServer(function(request,response) {
           response.end("404: Not Found: "+request.url)
       });
